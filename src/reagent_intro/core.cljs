e08dd671d369271bad56030361f1c0f95c7e20e5
Nigel Harsch <nigelharsch@gmail.com>
2017-04-23 16:41:10 -0400

    delete

--- src/reagent_intro/core.cljs
+++ src/reagent_intro/core.cljs
@@ -8,13 +8,8 @@
 ;chan is persisten queue in memory
 
 (defonce app-state
-  (reagent/atom {:message "hello from app state"
-                 :items [{:display "item 1"}
-                         {:display "item 2"}
-                         {:display "item 3"}
-                         {:display "item 4"}
-                         {:display "item 5"}
-                         ]
+  (reagent/atom {:message "Things to do"
+                 :items []
                  :active-item {}}))
 
 ; sort like reducers
@@ -27,6 +22,15 @@
   [item-text]
   (swap! app-state update-in [:items]
     (fn [items] (conj items {:display item-text}))))
+
+
+(defn delete-item
+  [idx]
+  (if (< idx (count (:items @app-state)))
+    (swap! app-state update-in [:items]
+                       (fn [items]
+                         (into [] (concat (subvec items 0 idx)
+                           (subvec items (inc idx))))))))
 
 (def EVENTS
   {:update-active-item update-active-item
@@ -69,14 +73,22 @@
 (defn items-list [EVENTCHANNEL items active-item]
    "will display items update an event channel when clicked"
    [:div {:class "items-list"}
-    (for [item (:items @app-state)]
-      ^{:key (:display item)}
-      [:div {:class (if (= active-item item) "item active" "item")}
-       [:p
-        {:on-click
-         (fn [event]
-           (put! EVENTCHANNEL [:update-active-item  {:active-item item}]))}
-        (:display item)]])])
+    ;; TODO could this be cleaner?
+    (map-indexed
+      (fn
+        [index item]
+        ^{:key index}
+        [:div {:class (if (= active-item item) "item active" "item")}
+         [:p
+          {:on-click
+           (fn [event]
+             (put! EVENTCHANNEL [:update-active-item  {:active-item item}]))}
+          (:display item)
+          [:a {:style {:color "red"} :onClick #(delete-item index) 
+                  :dangerouslySetInnerHTML {:__html "&nbsp;&#x2716;"}}]]
+         ])
+      (:items @app-state)
+      )])
 
 (defn app []
   [:div {:class "container"}
